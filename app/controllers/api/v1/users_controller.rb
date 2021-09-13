a6b46fc7d9cd0b353682ecf8489222ff96701542
Talaat Magdy <talaatmagdy75@gmail.com>
2021-09-13 02:35:13 +0200

    Adds update action the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[show update]
+
   # GET /users/1
   def show
-    render json: User.find(params[:id])
+    render json: @user
   end
 
   # POST /users
@@ -14,6 +16,15 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  # PATCH/PUT /users/1
+  def update
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
   # Only allow a trusted parameter "white list" through.
@@ -21,4 +32,7 @@ class Api::V1::UsersController < ApplicationController
     params.require(:user).permit(:email, :password)
   end
 
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -1,10 +1,12 @@
-require "test_helper"
+require 'test_helper'
 
 class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
   end
-  test "should show user" do
+
+  # show user
+  test 'should show user' do
     get api_v1_user_url(@user), as: :json
     assert_response :success
     # Test to ensure response contains the correct email
@@ -12,16 +14,27 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     assert_equal @user.email, json_response['email']
   end
 
-  test "should create user" do
+  # create new user
+  test 'should create user' do
     assert_difference('User.count') do
       post api_v1_users_url, params: { user: { email: 'test@test.org', password: '123456' } }, as: :json
     end
     assert_response :created
   end
-  test "should not create user with taken email" do
+  test 'should not create user with taken email' do
     assert_no_difference('User.count') do
       post api_v1_users_url, params: { user: { email: @user.email, password: '123456' } }, as: :json
     end
+    assert_response :unprocessable_entity
+  end
+
+  # update user
+  test 'should update user' do
+    patch api_v1_user_url(@user), params: { user: { email: @user.email, password: '123456' } }, as: :json
+    assert_response :success
+  end
+  test 'should not update user when invalid params are sent' do
+    patch api_v1_user_url(@user), params: { user: { email: 'bad_email', password: '123456' } }, as: :json
     assert_response :unprocessable_entity
   end
 end
