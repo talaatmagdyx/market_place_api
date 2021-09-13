0fe6eeb87b5ffca7b4eeca6b6efbb359067d25ce
Talaat Magdy <talaatmagdy75@gmail.com>
2021-09-13 02:39:33 +0200

    Adds destroy action to the users controller

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -37,4 +37,12 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     patch api_v1_user_url(@user), params: { user: { email: 'bad_email', password: '123456' } }, as: :json
     assert_response :unprocessable_entity
   end
+  
+  # Delete user
+  test 'should destroy user' do
+    assert_difference('User.count', -1) do
+      delete api_v1_user_url(@user), as: :json
+    end
+    assert_response :no_content
+  end
 end
